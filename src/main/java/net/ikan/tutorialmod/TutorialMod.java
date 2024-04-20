f61fd074b4baf885f52e0fb32242ec89d24601ab
alimulap <mohammad4limul@gmail.com>
2024-04-20 13:03:01 +0700

    Add cutom item + a bit refactor

--- src/main/java/net/ikan/tutorialmod/TutorialMod.java
+++ src/main/java/net/ikan/tutorialmod/TutorialMod.java
@@ -2,16 +2,18 @@ package net.ikan.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.ikan.tutorialmod.item.ModItemGroups;
+import net.ikan.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
-    public static final Logger LOGGER = LoggerFactory.getLogger("tutorial-mod");
+    public static final Logger LOGGER = LoggerFactory.getLogger("tutorialmod");
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		ModItemGroups.registerItemgGroups();
 	}
 }
